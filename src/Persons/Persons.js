5e7d624f12f78166dbda1d4402edba372c76d50a
Marko Mustač <mmustac@tria.hr>
2019-11-05 15:58:19 +0100

    added table screen

--- src/Persons/Persons.js
+++ src/Persons/Persons.js
@@ -32,6 +32,115 @@ export default class Persons {
 			region: "CA",
 			phoneNumber: "408-2222222",
 			emailAddress: "tomhanks@gmail.com"
+		},
+		{
+			userId: "jaugustus0",
+			jobTitleName: "Information Systems Manager",
+			firstName: "Judah",
+			lastName: "Augustus",
+			preferredFullName: "Judah Augustus",
+			employeeCode: "169.126.8.71",
+			region: "PT",
+			phoneNumber: "402-212-2307",
+			emailAddress: "jaugustus0@upenn.edu"
+		},
+		{
+			userId: "bmyott1",
+			jobTitleName: "Web Developer I",
+			firstName: "Becky",
+			lastName: "Myott",
+			preferredFullName: "Becky Myott",
+			employeeCode: "68.72.53.223",
+			region: "ID",
+			phoneNumber: "315-695-2264",
+			emailAddress: "bmyott1@patch.com"
+		},
+		{
+			userId: "pbikker2",
+			jobTitleName: "Biostatistician I",
+			firstName: "Parry",
+			lastName: "Bikker",
+			preferredFullName: "Parry Bikker",
+			employeeCode: "227.219.98.24",
+			region: "SE",
+			phoneNumber: "330-364-8339",
+			emailAddress: "pbikker2@cornell.edu"
+		}, {
+			userId: "acrompton3",
+			jobTitleName: "Senior Cost Accountant",
+			firstName: "Abdul",
+			lastName: "Crompton",
+			preferredFullName: "Abdul Crompton",
+			employeeCode: "148.201.180.216",
+			region: "CN",
+			phoneNumber: "633-686-9856",
+			emailAddress: "acrompton3@time.com"
+		},
+		{
+			userId: "gbradtke4",
+			jobTitleName: "Account Representative II",
+			firstName: "Galvan",
+			lastName: "Bradtke",
+			preferredFullName: "Galvan Bradtke",
+			employeeCode: "55.208.122.44",
+			region: "CN",
+			phoneNumber: "899-712-5659",
+			emailAddress: "gbradtke4@oaic.gov.au"
+		},
+		{
+			userId: "ebernaciak5",
+			jobTitleName: "VP Sales",
+			firstName: "Elvyn",
+			lastName: "Bernaciak",
+			preferredFullName: "Elvyn Bernaciak",
+			employeeCode: "13.207.55.28",
+			region: "CN",
+			phoneNumber: "377-879-9940",
+			emailAddress: "ebernaciak5@yahoo.co.jp"
+		},
+		{
+			userId: "aklewi6",
+			jobTitleName: "Editor",
+			firstName: "Audrie",
+			lastName: "Klewi",
+			preferredFullName: "Audrie Klewi",
+			employeeCode: "20.157.61.165",
+			region: "CN",
+			phoneNumber: "182-976-7139",
+			emailAddress: "aklewi6@constantcontact.com"
+		},
+		{
+			userId: "smcclosh7",
+			jobTitleName: "Systems Administrator I",
+			firstName: "Sibeal",
+			lastName: "McClosh",
+			preferredFullName: "Sibeal McClosh",
+			employeeCode: "59.37.11.68",
+			region: "PT",
+			phoneNumber: "343-924-3541",
+			emailAddress: "smcclosh7@nymag.com"
+		},
+		{
+			userId: "lbillin8",
+			jobTitleName: "Legal Assistant",
+			firstName: "Lani",
+			lastName: "Billin",
+			preferredFullName: "Lani Billin",
+			employeeCode: "0.44.147.210",
+			region: "RU",
+			phoneNumber: "796-397-4655",
+			emailAddress: "lbillin8@people.com.cn"
+		},
+		{
+			userId: "dwillcock9",
+			jobTitleName: "Programmer IV",
+			firstName: "Derby",
+			lastName: "Willcock",
+			preferredFullName: "Derby Willcock",
+			employeeCode: "165.218.38.131",
+			region: "ID",
+			phoneNumber: "247-381-5026",
+			emailAddress: "dwillcock9@moonfruit.com"
 		}
 	];
 }
