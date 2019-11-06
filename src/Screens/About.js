b5fcac95e18b698fae946ab6ad2936ca99263ac8
Vanda Karabajic <vkarabajic@tria.hr>
2019-11-06 15:46:11 +0100

    vklkda

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../HeaderFooter/Header'
 import Footer from '../HeaderFooter/Footer'
-import { Container, Button, Form, FormGroup, Label, Input, Row, Col, Alert } from 'reactstrap';
+import { Container, Button, Form, FormGroup, Label, Input, Row, Col, Alert, FormFeedback } from 'reactstrap';
 
 export default class About extends Component {
 
@@ -9,7 +9,10 @@ export default class About extends Component {
 		super(props)
 
 		this.state = {
-			toggle: false
+			toggle: false,
+			validEmail: null,
+			validUserName: null,
+			validMessage: null
 		};
 	}
 
@@ -24,25 +27,41 @@ export default class About extends Component {
 							<Form style={{ margin: "30px 0px" }}>
 								<FormGroup>
 									<Label for="emailId">Email</Label>
-									<Input type="email" name="email" id="emailId" placeholder="Dundie@is-the.best" />
+									<Input type="email" name="email" id="emailId" placeholder="Dundie@is-the.best" valid={this.state.validEmail} invalid={!this.state.validEmail}
+										onChange={(e) => {
+											e.target.value.length > 0 ? this.setState({ validEmail: true }) : this.setState({ validEmail: false })
+										}} />
 								</FormGroup>
 								<FormGroup>
 									<Label for="usernameId">Username</Label>
-									<Input type="username" name="username" id="usernameId" placeholder="Dundie 🔥" />
+									<Input type="username" name="username" id="usernameId" placeholder="Dundie 🔥" valid={this.state.validUserName} invalid={!this.state.validUserName}
+										onChange={(e) => {
+											e.target.value.length > 0 ? this.setState({ validUserName: true }) : this.setState({ validUserName: false })
+										}} />
 								</FormGroup>
 								<FormGroup>
 									<Label for="exampleId">Message</Label>
-									<Input type="textarea" name="text" id="exampleId" placeholder="Please send us your message 😄" />
+									<Input type="textarea" name="text" id="exampleId" placeholder="Please send us your message 😄" valid={this.state.validMessage} invalid={!this.state.validMessage}
+										onChange={(e) => {
+											e.target.value.length > 0 ? this.setState({ validMessage: true }) : this.setState({ validMessage: false })
+										}} />
 								</FormGroup>
 							</Form>
 							<Button block style={{ margin: "20px 0" }} onClick={() => { this.setState({ toggle: true }) }} color="primary">Submit</Button>
 						</Col>
 						<Col xs="4">
-							<Alert color="success" isOpen={this.state.toggle} toggle={() => {
+							<Alert color={
+								this.state.validEmail &&
+									this.state.validUserName &&
+									this.state.validMessage ?
+									"success" : "danger"
+							} isOpen={this.state.toggle} toggle={() => {
 								this.setState({ toggle: !this.state.toggle })
 							}}>
-								Form submited successfuly
-     					 </Alert>
+								{this.state.validEmail &&
+									this.state.validUserName &&
+									this.state.validMessage ? "Form submited successfuly" : "All fields are required"}
+							</Alert>
 						</Col>
 					</Row>
 				</Container>
